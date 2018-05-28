8041be28509926d86933cb2bcd67c5f850d8fa4a
Neommastermind <31413602+Neommastermind@users.noreply.github.com>
2018-05-27 22:25:40 -0600

    Enhancements and build
    Bullet enhancements for the player and enemies have been made.

--- Space Shooter/Assets/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Assets/Scripts/PlayerController.cs
@@ -27,18 +27,48 @@ public class PlayerController : MonoBehaviour {
 	public Transform shotSpawn;
 	//When the player can shoot another bolt
 	private float nextFire;
+	//A count to keep track of which shot type to use
+	private int shotCount;
 
 	void Start() {
 		//Get the Rigidbody component
 		rb = GetComponent<Rigidbody>();
 		//Get the Audio Source component
 		audioSource = GetComponent<AudioSource>();
+		//Initialize the shotCount
+		shotCount = 0;
 	}
 
 	void Update() {
 		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			//Update the time of nextFire
 			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+
+			//Change the shot type depending on the count
+			if (shotCount == 0) {
+				//Single shot
+				Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			} 
+			else if (shotCount == 1) {
+				//Two shots
+				Instantiate (shot, shotSpawn.position, Quaternion.Euler (shotSpawn.rotation.x, shotSpawn.rotation.y + 30, shotSpawn.rotation.z));
+				Instantiate (shot, shotSpawn.position, Quaternion.Euler (shotSpawn.rotation.x, shotSpawn.rotation.y - 30, shotSpawn.rotation.z));
+			} 
+			else if (shotCount == 2) {
+				//Three shots
+				Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+				Instantiate (shot, shotSpawn.position, Quaternion.Euler (shotSpawn.rotation.x, shotSpawn.rotation.y + 30, shotSpawn.rotation.z));
+				Instantiate (shot, shotSpawn.position, Quaternion.Euler (shotSpawn.rotation.x, shotSpawn.rotation.y - 30, shotSpawn.rotation.z));
+			}
+
+			//Update the shot count
+			shotCount++;
+
+			//Reset the shot count if it exceeds the maximum
+			if (shotCount > 2) {
+				shotCount = 0;
+			}
+
 			//Play the shooting sounds
 			audioSource.Play();
 		}
--- Space Shooter/Assets/Assets/Scripts/WeaponController.cs
+++ Space Shooter/Assets/Assets/Scripts/WeaponController.cs
@@ -10,15 +10,34 @@ public class WeaponController : MonoBehaviour {
 	public float delay;
 
 	private AudioSource audioSource;
+	//The type of shots this enemy will use
+	private int shotType;
 
 	// Use this for initialization
 	void Start () {
 		audioSource = GetComponent<AudioSource> ();
+		shotType = Random.Range (0, 3);
 		InvokeRepeating ("Fire", delay, fireRate);
 	}
 
 	void Fire() {
-		Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		//Change the shot type depending on the count
+		if (shotType == 0) {
+			//Single shot
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		} 
+		else if (shotType == 1) {
+			//Two shots
+			Instantiate (shot, shotSpawn.position, Quaternion.Euler (shotSpawn.rotation.x, shotSpawn.rotation.y + 210, shotSpawn.rotation.z));
+			Instantiate (shot, shotSpawn.position, Quaternion.Euler (shotSpawn.rotation.x, shotSpawn.rotation.y - 210, shotSpawn.rotation.z));
+		} 
+		else if (shotType == 2) {
+			//Three shots
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			Instantiate (shot, shotSpawn.position, Quaternion.Euler (shotSpawn.rotation.x, shotSpawn.rotation.y + 210, shotSpawn.rotation.z));
+			Instantiate (shot, shotSpawn.position, Quaternion.Euler (shotSpawn.rotation.x, shotSpawn.rotation.y - 210, shotSpawn.rotation.z));
+		}
+
 		audioSource.Play ();
 	}
 
